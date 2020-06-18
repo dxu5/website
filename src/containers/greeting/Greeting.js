e2fe1f511a1f959b9d6713e61c061e82d08605fd
bio1600project <fzhao8@gmail.com>
2020-06-17 19:48:59 -0700

    initial commit

---
+++ src/containers/greeting/Greeting.js
@@ -0,0 +1,59 @@
+﻿import React from "react";
+import "./Greeting.css";
+import SocialMedia from "../../components/socialMedia/SocialMedia";
+import Button from "../../components/button/Button";
+import { greeting } from "../../portfolio";
+import { Fade } from "react-reveal";
+import emoji from "react-easy-emoji";
+import Typical from "react-typical";
+
+export default function Greeting() {
+  return (
+    <Fade bottom duration={1000} distance="40px">
+      <div className="greet-main" id="greeting">
+        <div className="greeting-main">
+          <div className="greeting-text-div">
+            <div>
+              <h1 className="greeting-text"> Hi, I'm Derek</h1>
+              <p className="greeting-text-p subTitle">
+                I am a{" "}
+                <Typical
+                  loop={Infinity}
+                  wrapper="b"
+                  steps={[
+                    "Full-stack Developer",
+                    1500,
+                    "Violinist",
+                    1500,
+                    "Leader",
+                    1500,
+                    "Web Developer",
+                    1500,
+                    "Leader",
+                    1500,
+                  ]}
+                />
+              </p>
+              <SocialMedia />
+              <div className="button-greeting-div">
+                <Button text="Contact me" href="#contact" />
+                <Button
+                  text="My Resume"
+                  newTab={true}
+                  href={greeting.resumeLink}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="greeting-image-div">
+            <img
+              className="profile-img"
+              alt="saad sitting on table"
+              src={require("../../assets/images/profile-picture.png")}
+            ></img>
+          </div>
+        </div>
+      </div>
+    </Fade>
+  );
+}
---
+++ src/portfolio.js
@@ -0,0 +1,328 @@
+﻿/* Change this file to get your personal Portfolio */
+
+// Your Summary And Greeting Section
+
+import emoji from "react-easy-emoji";
+
+const greeting = {
+  /* Your Summary And Greeting Section */
+  username: "Derek Xu",
+  title: "Hi all, I'm Derek",
+  subTitle: emoji(
+    "A passionate Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+  ),
+  resumeLink:
+    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing",
+};
+
+// Your Social Media Link
+
+const socialMediaLinks = {
+  github: "https://github.com/dxu5",
+  linkedin: "https://www.linkedin.com/in/derek-jinxiao-xu/",
+  gmail: "derek_xu@brown.edu",
+  instagram: "https://www.instagram.com/derekdjxx/",
+  // Instagram and Twitter are also supported in the links!
+};
+
+// Your Skills Section
+
+const skillsSection = {
+  title: "About Me",
+  subTitle: "Full-stack Web Developer, Violinist, and Gundam Enthusiast.",
+  skills: [
+    "As a person, I am curious about everything that is around me: from the smallest subatomic particle to the largest of skyscrapers. I always wonder how everything is linked together: how they compliment each other and how they clash. I make it my mission to understand as much as I can, while doing every task to the best of my ability: whether it is in Computer Science, Biology, Biotechnology, or Music. I cannot wait to learn something new and I hope you will be a part of my journey.",
+    "I am currently looking for opportunities for Software Engineering positions. I am a motivated and quick learner, one who is always excited to experiment and understand new technologies. I am interested in many different aspects of software: from web development to machine learning.",
+  ],
+
+  /* Make Sure You include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "Biology",
+      fontAwesomeClassname: "fas fa-vials",
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt",
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "fab fa-sass",
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js",
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react",
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "fab fa-node",
+    },
+    {
+      skillName: "swift",
+      fontAwesomeClassname: "fab fa-swift",
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "fab fa-npm",
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database",
+    },
+    {
+      skillName: "aws",
+      fontAwesomeClassname: "fab fa-aws",
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "fas fa-fire",
+    },
+    {
+      skillName: "python",
+      fontAwesomeClassname: "fab fa-python",
+    },
+    {
+      skillName: "docker",
+      fontAwesomeClassname: "fab fa-docker",
+    },
+  ],
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend/Design", //Insert stack or technology you have experience in
+      progressPercentage: "90%", //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Backend",
+      progressPercentage: "70%",
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "60%",
+    },
+  ],
+};
+
+// Your top 3 work experiences
+
+const workExperiences = {
+  viewExperiences: true, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "Full-Stack Engineer",
+      company: "Full-Stack At Brown",
+      companylogo: require("./assets/images/0.jpeg"),
+      date: "Feb 2020 – Present",
+      descBullets: [
+        "Created web application for the East Coast Taiko Conference using react, redux, html, css, and javascript",
+        "Engineered and established website and technologies used for the 2020 Hack At Home Hackathon",
+        "Led multiple workshops during hackathon focused on relevant web technologies such as React and Javascript",
+      ],
+    },
+    {
+      role: "Research Technician",
+      company: "Brown University Yajima Lab",
+      companylogo: require("./assets/images/Brown-University-logo.jpg"),
+      date: "Sep 2017 – May 2020",
+      descBullets: [
+        "Published as Co-First Author in Developmental Dynamics",
+        "Led research team of 6 to test and understand plasticity regulation (Germline factors DCLK1 and RB1) by using sea urchin",
+        "Extensive knowledge in various wet lab techniques: PCR, Western Blot, Immunoprecipitation, and Immunofluorescence",
+        "Used Python, ImageJ, Confocal microscopy and Prism in order to track and calculate cancer cell pattern movement in cell culture, as well as the factors that control this type of movement",
+      ],
+    },
+    {
+      role: "Computer Science Instructor",
+      company: "GoPeer",
+      companylogo: require("./assets/images/gopeer.jpeg"),
+      date: "Sep 2019 – Present",
+      descBullets: [
+        "Provided instruction on fundamental computer science concepts and math topics to high school students (30+ students) interested in computer science (Languages taught: Python, Javascript, and Java)",
+        "Developed project-based curriculum for students to master and understand difficult programming and math topics such as building tetris, websites, and scratch programs.",
+      ],
+    },
+  ],
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  githubConvertedToken: process.env.REACT_APP_GITHUB_TOKEN,
+  githubUserName: "saadpasta", // Change to your github username to view your profile in Contact Section.
+  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to false
+};
+
+// Some Big Projects You have worked with your company
+
+const bigProjects = {
+  title: "Big Projects",
+  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  projects: [
+    {
+      image: "https://drive.google.com/uc?id=1exWn9T6j8TsfDDHJnS3VR66eP6RiGAfY",
+      link: "http://saayahealth.com/",
+    },
+    {
+      image: "https://drive.google.com/uc?id=1MXoXcQRK-pH8J82wyjCW4SJk5AxJe7tf",
+      link: "http://nextu.se/",
+    },
+  ],
+};
+
+// Your Achievement Section Include Your Certification Talks and More
+
+const achievementSection = {
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle:
+    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+
+  achivementsCards: [
+    {
+      title: "Google Code-In Finalist",
+      subtitle:
+        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
+      image:
+        "https://1.bp.blogspot.com/-Ig-v1tDXZt4/XDODmZvWp1I/AAAAAAAAB0A/KtbFdBPFVQw2O15FekkIR0Yg8MUp--rngCLcBGAs/s1600/GCI%2B-%2BVertical%2B-%2BGray%2BText%2B-%2BWhite%2BBG.png",
+      footerLink: [
+        {
+          name: "Certification",
+          url:
+            "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing",
+        },
+        {
+          name: "Award Letter",
+          url:
+            "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing",
+        },
+        {
+          name: "Google Code-in Blog",
+          url:
+            "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html",
+        },
+      ],
+    },
+    {
+      title: "Google Assistant Action",
+      subtitle:
+        "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Google_Assistant_logo.svg/1200px-Google_Assistant_logo.svg.png",
+      footerLink: [
+        {
+          name: "View Google Assistant Action",
+          url:
+            "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en",
+        },
+      ],
+    },
+
+    {
+      title: "PWA Web App Developer",
+      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu93Bd6LDbxPEOXr-hfLqLYzmHny8c0MJoI3exQP-lwpFLRT7g&s",
+      footerLink: [
+        { name: "Certification", url: "" },
+        {
+          name: "Final Project",
+          url: "https://pakistan-olx-1.firebaseapp.com/",
+        },
+      ],
+    },
+  ],
+};
+
+// Blogs Section
+
+const blogSection = {
+  title: "Blogs",
+  subtitle:
+    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+
+  blogs: [
+    {
+      url:
+        "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      description:
+        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?",
+    },
+    {
+      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+      title: "Why REACT is The Best?",
+      description:
+        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies.",
+    },
+  ],
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji(
+    "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
+  ),
+
+  talks: [
+    {
+      title: "Build Actions For Google Assistant",
+      subtitle: "Codelab at GDG DevFest Karachi 2019",
+      slides_url: "https://bit.ly/saadpasta-slides",
+      event_url: "https://www.facebook.com/events/2339906106275053/",
+      image:
+        "https://scontent.fkhi6-1.fna.fbcdn.net/v/t1.0-9/76714032_1730516240415559_1293494289556307968_o.jpg?_nc_cat=103&_nc_ohc=s5f81rdZd6wAQmqUa52YQBA21MjVOy7e-HN9jI7MspRDC-v5ahEouyInQ&_nc_ht=scontent.fkhi6-1.fna&oh=c29141a9a0de7ff2011a8191f5475a78&oe=5E83127C",
+    },
+  ],
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo",
+  ],
+};
+
+const contactInfo = {
+  title: "Don't Hesitate To Connect With Me!",
+  subtitle: "Have Some Questions Or Just Want To Chat? Visit The Links Below!",
+  email_address: "derek_xu@brown.edu",
+};
+
+//Twitter Section
+
+const twitterDetails = {
+  userName: "twitter", //Replace "twitter" with your twitter username without @
+};
+export {
+  greeting,
+  socialMediaLinks,
+  skillsSection,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails,
+};
