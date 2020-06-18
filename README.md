d729e1bf47cc8e0fb20bda5479621341af87e208
bio1600project <fzhao8@gmail.com>
2020-06-17 21:29:50 -0700

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-Hello World!
-Welcome to my website!
-My name is Derek Xu and I am a recent graduate from Brown University.
-I am an avid software developer and am always curious about how the world
-functions around me.
-I am currently seeking opportunities in software engineeing.
-Look forward to connecting with you and thank you for visiting my website!
-Best,
-Derek Xu
---
+++ precache-manifest.06518b14b603ca76c19e410a3be0aa38.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fefaa2197eca47ea045f56ddae10a0e8",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "98e63daa2830a9a9d0ba",
+    "url": "/website/static/css/main.dd71082a.chunk.css"
+  },
+  {
+    "revision": "b855dac8632b8e3be6e7",
+    "url": "/website/static/js/2.ae7ae400.chunk.js"
+  },
+  {
+    "revision": "98e63daa2830a9a9d0ba",
+    "url": "/website/static/js/main.4c5d259a.chunk.js"
+  },
+  {
+    "revision": "688ede03bf4a2e88a86d",
+    "url": "/website/static/js/runtime-main.17bfc0be.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/website/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "aa2b294ac1758698188bb94cdcfa2472",
+    "url": "/website/static/media/Brown-University-logo.aa2b294a.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/website/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "9436f4d0910af135d018d71e4c376e2b",
+    "url": "/website/static/media/covid3.9436f4d0.png"
+  },
+  {
+    "revision": "aaef3e19c1b6b38efda57841b8365153",
+    "url": "/website/static/media/derekfb.aaef3e19.jpg"
+  },
+  {
+    "revision": "3ba289f93a97890bb0ae3351f37d5c08",
+    "url": "/website/static/media/dxsorting2.3ba289f9.png"
+  },
+  {
+    "revision": "1f841d82004551a8fd3883a0649ade11",
+    "url": "/website/static/media/dxvoyage2.1f841d82.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/website/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "37e592653034f2c3ab3146f04cc4a289",
+    "url": "/website/static/media/profile-picture.37e59265.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/website/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/website/static/media/talksCardBack.0288c5ec.svg"
+  }
+]);
